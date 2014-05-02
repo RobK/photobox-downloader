c6fc2b4cb1af43463ba6f39276c2e9ecf0cc68ef
Robert Kehoe <robert@robertkehoe.com>
2014-05-02 21:58:27 +0300

    Adding new download single photo feature. Improving login failure detection and handling.

--- example.js
+++ example.js
@@ -3,17 +3,28 @@
  */
 
 var config = require('./config');
-var photoBox = require('./photobox.js');
+var photoBox = require('./photobox');
 
 //console.log(photoBox);
 
 
-photoBox.login(config, function (err, result) {
+photoBox.login(config, function (err) {
   if (err) {
     console.log('ERROR! Something went wrong logging in!');
     console.log(err);
   } else {
-    console.log(photoBox.getAlbumList());
+    console.log('Logged in!!!');
+    photoBox.downloadPhoto({
+      id: '20259833515',
+      outputDir : __dirname + '/out'
+    }, function (err) {
+      if (err) {
+        console.log('ERROR! Something went wrong downloading image!');
+        console.log(err);
+      } else {
+        console.log('Photo has been downloaded to the outputDir');
+      }
+    });
   }
 
 });
--- photobox.js
+++ photobox.js
@@ -6,21 +6,30 @@ var cheerio = require('cheerio');
 var request;
 var config;
 var html;
+var fs = require('fs');
 
 module.exports = {
   login : function login(configIn, callback) {
+    var j = request.jar();
+    var cookie = 'pbx_www_photobox_ie="' +  config.cookieValue + '";path=/;expires=' +
+      new Date("6/1/2014	21:08:38") + ';';
     config = configIn;
     request = require('request');
-    var j = request.jar();
-    j.setCookie('pbx_' + config.baseDomain.replace('.', '_') + '=' + config.cookieValue, 'http://' + config.baseDomain);
-    request = request.defaults({jar : j});
+    j.setCookie(cookie, 'http://' + config.baseDomain);
 
-    request('http://localhost/photobox-downloader/mock/albumsList.html', function (error, response, body) {
-      if (!error && response.statusCode == 200) {
-        html = body;
-        callback(null, true);
+    request = request.defaults({jar : j});
+    request('http://' + config.baseDomain + '/my/albums', function (err, response, body) {
+//    request('http://localhost/photobox-downloader/mock/albumsList.html', function (error, response, body) {
+      if (!err && response.statusCode == 200) {
+        var $ = cheerio.load(body);
+        if ($('#pbx_signout').length !== 0) {
+          html = body;
+          callback(null, true);
+        } else {
+          callback('Was unable to login with given cookie value!');
+        }
       } else {
-        error(error);
+        callback(err);
       }
     });
 
@@ -32,10 +41,10 @@ module.exports = {
 
   getAlbumList : function getAlbumList() {
     var $ = cheerio.load(html);
-
     var albumObjects = [];
 
     var albums = $('.pbx_myphotobox_thumbnail');
+    //console.log(albums);
     albums.each(function () {
 
       albumObjects.push({
@@ -64,7 +73,23 @@ module.exports = {
 
   },
 
-  downloadPhoto : function downloadPhotos(photoId, path) {
+  downloadPhoto : function downloadPhotos(options, callback) {
 
+    request('http://' + config.baseDomain + '/my/photo/full?photo_id=' + options.id,
+      function (err, response, body) {
+        if (!err && response.statusCode == 200) {
+          var $ = cheerio.load(body);
+          var name = $('img').attr('alt');
+          if (name.match(/\./) === null) {
+            name = name + '.jpg';
+          }
+
+          request($('img').attr('src'), callback).pipe(fs.createWriteStream(options.outputDir + '/' + name));
+
+        } else {
+          callback(err);
+        }
+      }
+    );
   }
 };
